1689c806df30f05730bd3e7e165dbdf12a0f8f76
Deverror 6068 <113009666+deverror6068@users.noreply.github.com>
2023-12-16 18:04:22 +0100

    Portage tu projet vers la version 0.7.9.9
    Ajout de la version 0.7.9.9 du projet
    • Ajout du champs "type de tri" dans la base de donnée
    • ajout de la prise en charge du champs dans le code
    •corrections  en tout genre dans le code (syntaxe, pertinence,bonne pratiques...)

---
+++ src/Api/index.php
@@ -0,0 +1,25 @@
+<?php
+require_once "endpoint.php" ;
+require_once "security\SecLib.php";
+require_once "LinkLib.php";
+require_once "dbhandler.php";
+
+
+ENDPOINT::routing();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
